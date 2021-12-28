10ea8b3acacb3bfaf5fea46a23382370d2cce210
zhiminzhang0830 <95690929+zhiminzhang0830@users.noreply.github.com>
2021-12-28 19:45:10 +0800

    Update export_model.py

--- tools/export_model.py
+++ tools/export_model.py
@@ -18,6 +18,7 @@ import sys
 __dir__ = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(__dir__)
 sys.path.append(os.path.abspath(os.path.join(__dir__, "..")))
+sys.path.append('/home/aistudio/external-libraries')
 
 import argparse
 
